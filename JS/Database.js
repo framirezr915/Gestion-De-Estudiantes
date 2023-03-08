9b8569b9361856492c7246ba9048a4ff36266fa7
lpdondi <lpdondi@gmail.com>
2023-03-08 12:25:10 -0600

    Logica de la base
    Agrega la logica para jalar de la base de datos

---
+++ JS/Database.js
@@ -0,0 +1,36 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+import { getDatabase,get,ref,child } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";  
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6z7vPeIDhq7MQrV7M6sZOq2rFn_0ys6Q",
+  authDomain: "gestion-de-estudiantes.firebaseapp.com",
+  projectId: "gestion-de-estudiantes",
+  storageBucket: "gestion-de-estudiantes.appspot.com",
+  messagingSenderId: "195992130911",
+  appId: "1:195992130911:web:d883094ec0cd5d115612ea",
+  measurementId: "G-MZ74HG8FX8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getDatabase();
+ 
+function findData(){
+    const dbref = ref(db);
+
+    get(child(dbref, "Universidad/" + "Cursos/" + "Curso1"))
+    .then((snapshot)=>{
+        if(snapshot.exists()){
+console.log(snapshot.val().CursoID);
+        }else{
+            console.log("No data found");
+        }
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+};
+
+findData();
