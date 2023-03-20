5b4a27d0abc296102a73b7a26dddee52c7c60299
lmontero18 <lmonterom879@gmail.com>
2023-03-19 22:58:03 -0600

    Estudiantes complete

--- JS/Estudiantes.js
+++
@@ -1,8 +0,0 @@
-const dropdownBtns = document.querySelectorAll(".dropdown-btn");
-
-dropdownBtns.forEach(function(btn) {
-  btn.addEventListener("click", function() {
-    const container = this.nextElementSibling;
-    container.classList.toggle("active");
-  });
-});
