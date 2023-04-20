067cdde16c563ef3f83cf7c7bbfeccf7a428405a
lmontero18 <lmonterom879@gmail.com>
2023-04-19 18:28:11 -0600

    asistencia working :D

--- JS/asistencia.js
+++ JS/asistencia.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
 import {getFirestore, collection, addDoc, getDocs, onSnapshot } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
-import { getDatabase, ref, onValue, push, set, remove} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
-import { updateDoc, doc } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
+import { getDatabase, ref, onValue, push, set, remove, update, get} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
+import { updateDoc, doc} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD6z7vPeIDhq7MQrV7M6sZOq2rFn_0ys6Q",
@@ -17,69 +17,50 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-const dataRef = ref(db, "Cursos/5");
+const cursoId = sessionStorage.getItem("ID");
+const dataRef = ref(db, "Cursos/" + cursoId);
 
 onValue(dataRef, (snapshot) => {
   const estudiantes = snapshot.val();
-
   const estudiantesTable = document.querySelector("#estudiantesTable tbody");
 
-//Crear una fila para cada estudiante y colocar su nombre en la primera celda
-for (const key in estudiantes) {
-  const estudiante = estudiantes[key];
-  const tr = document.createElement("tr");
-  const nombreTd = document.createElement("td");
-  nombreTd.textContent = `${estudiante.Nombre} ${estudiante.Apellidos}`;
-  nombreTd.dataset.id = key;
-  tr.appendChild(nombreTd);
+  for (const key in estudiantes) {
+    const estudiante = estudiantes[key];
+    const tr = document.createElement("tr");
+    const nombreTd = document.createElement("td");
+    nombreTd.textContent = `${estudiante.Nombre} ${estudiante.Apellidos}`;
+    nombreTd.dataset.id = key;
+    tr.appendChild(nombreTd);
 
-  //Agregar una celda con un checkbox para cada semana
-  for (let i = 1; i <= 15; i++) {
-    const semanaTd = document.createElement("td");
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    if (estudiante[`Semana${i}`]) {
-      checkbox.checked = true;
-    }
-    semanaTd.appendChild(checkbox);
-    tr.appendChild(semanaTd);
-  }
+    for (let i = 1; i <= 15; i++) {
+      const semanaTd = document.createElement("td");
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.dataset.semana = i;
 
-  estudiantesTable.appendChild(tr);
-}
-
-const guardarBtn = document.querySelector("#Guardarbtn");
-guardarBtn.addEventListener("click", () => {
-  const estudiantesTable = document.querySelector("#estudiantesTable tbody");
-  const filas = estudiantesTable.querySelectorAll("tr");
-
-  // Crear una colección para la asistencia
-  const asistenciaCollection = collection(db, "Asistencia");
-
-  // Recorrer cada fila de la tabla
-  filas.forEach((fila) => {
-    const nombreTd = fila.querySelector("td:first-child");
-    const nombre = nombreTd.textContent.trim();
-
-    // Crear un objeto con los datos del estudiante
-    const estudiante = { Nombre: nombre };
-
-    // Recorrer cada checkbox de la fila y guardar su valor en el objeto del estudiante
-    const checkboxes = fila.querySelectorAll("td input[type='checkbox']");
-    checkboxes.forEach((checkbox, index) => {
-      estudiante[`Semana${index + 1}`] = checkbox.checked;
-    });
-
-    // Guardar el objeto del estudiante en Firebase
-    addDoc(asistenciaCollection, estudiante)
-      .then(() => {
-        console.log("Asistencia guardada correctamente");
-      })
-      .catch((error) => {
-        console.error("Error al guardar la asistencia: ", error);
+      const asistenciaRef = ref(db, `Asistencia/${cursoId}/${key}/Semana${i}`);
+      get(asistenciaRef).then((snapshot) => {
+        const asistencia = snapshot.val();
+        checkbox.checked = asistencia === true;
+      }).catch((error) => {
+        console.error("Error al obtener la asistencia: ", error);
       });
-  });
-});
 
+      semanaTd.appendChild(checkbox);
+      tr.appendChild(semanaTd);
 
+      checkbox.addEventListener("change", (e) => {
+        const estudianteId = e.target.closest("tr").querySelector("td:first-child").dataset.id;
+        const semana = e.target.dataset.semana;
+        const asistenciaRef = ref(db, `Asistencia/${cursoId}/${estudianteId}/Semana${semana}`);
+        set(asistenciaRef, e.target.checked).then(() => {
+          console.log("Asistencia guardada correctamente");
+        }).catch((error) => {
+          console.error("Error al guardar la asistencia: ", error);
+        });
+      });
+    }
+
+    estudiantesTable.appendChild(tr);
+  }
 });
